d1daa9fef20563d22e858924469818a0ac0b0b90
Szymonidas1112 <84992086+Szymonidas1112@users.noreply.github.com>
2021-05-29 13:28:30 +0100

    Update blabla.lua

--- blabla.lua
+++ blabla.lua
@@ -3,13 +3,14 @@ if game.PlaceId == 5774073359 then
     local Window = Library.CreateLib("Eat Shit Simulator", "Sentinel")
     local Tab = Window:NewTab("Main")
     local TeleportsSection = Tab:NewSection("Teleports")
-    TeleportsSection:NewButton("ATM", "tp to atm", function(v)
+    local TogglesSection = Tab:NewSection("Toggles")
+    TeleportsSection:NewButton("ATM", "Teleports to Atm", function(v)
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.ATM.CFrame
     end)
-    TeleportsSection:NewButton("Chest", "tp to chest", function(v)
+    TeleportsSection:NewButton("Chest", "Teleports to chest", function(v)
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.Chest.CFrame
     end)
-    TeleportsSection:NewToggle("TogglePunch", "Loop Punch", function(state)
+    TogglesSection:NewToggle("TogglePunch", "Loop Punch", function(state)
     if state then
     _G.Toggle = true
 
@@ -33,7 +34,7 @@ if game.PlaceId == 5774073359 then
     end
     end
 end)
-TeleportsSection:NewToggle("Toggle Rebirth", "Loop Rebirth", function(state)
+TogglesSection:NewToggle("Toggle Rebirth", "Loop Rebirth", function(state)
     if state then
         _G.Toggle = true
 
@@ -63,5 +64,109 @@ PlayerSection:NewSlider("JumpPower", "Changes jumppower", 500, 0, function(s) --
 end)
 PlayerSection:NewButton("Set Original JumpPower", "Sets jumppower to original", function()
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
+end)
+TeleportsSection:NewButton("Safe", "Teleports to safe", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.Safe.CFrame
+end)
+TeleportsSection:NewButton("Sand Mound", "Teleports to Sand Mound", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.SandMound.CFrame
+end)
+TeleportsSection:NewButton("Sand Castle", "Teleports to Sand Castle", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.SandCastle.CFrame
+end)
+TeleportsSection:NewButton("Monster Plus", "Teleports to Monster Plus", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.MonsterPlus.Handle.CFrame
+end)
+PlayerSection:NewButton("Ctrl Tp", "Hold ctrl and click to tp", function()
+local Plr = game:GetService("Players").LocalPlayer
+local Mouse = Plr:GetMouse()
+
+Mouse.Button1Down:connect(function()
+if not game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.LeftControl) then return end
+if not Mouse.Target then return end
+mouse = game.Players.LocalPlayer:GetMouse()
+local pos = mouse.Hit+Vector3.new(0,2.5,0)
+pos = CFrame.new(pos.X,pos.Y,pos.Z)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
+end)
+end)
+PlayerSection:NewButton("Toggle Fly", "Press E to toggle fly", function()
+    repeat wait() until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:findFirstChild("Torso") and game.Players.LocalPlayer.Character:findFirstChild("Humanoid")
+local mouse = game.Players.LocalPlayer:GetMouse()
+repeat wait() until mouse
+  local plr = game.Players.LocalPlayer
+  local torso = plr.Character.Torso
+  local flying = true
+  local deb = true
+  local ctrl = {f = 0, b = 0, l = 0, r = 0}
+  local lastctrl = {f = 0, b = 0, l = 0, r = 0}
+  local maxspeed = 50
+  local speed = 0
+
+  function Fly()
+    local bg = Instance.new("BodyGyro", torso)
+    bg.P = 9e4
+    bg.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+    bg.cframe = torso.CFrame
+    local bv = Instance.new("BodyVelocity", torso)
+    bv.velocity = Vector3.new(0,0.1,0)
+    bv.maxForce = Vector3.new(9e9, 9e9, 9e9)
+    repeat wait()
+      if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then
+        speed = speed+.5+(speed/maxspeed)
+        if speed > maxspeed then
+          speed = maxspeed
+        end
+      elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then
+        speed = speed-1
+        if speed < 0 then
+          speed = 0
+        end
+      end
+      if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then
+        bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
+        lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r}
+      elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then
+        bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
+      else
+        bv.velocity = Vector3.new(0,0.1,0)
+      end
+      bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0)
+    until not flying
+    ctrl = {f = 0, b = 0, l = 0, r = 0}
+    lastctrl = {f = 0, b = 0, l = 0, r = 0}
+    speed = 0
+    bg:Destroy()
+    bv:Destroy()
+  end
+  mouse.KeyDown:connect(function(key)
+  if key:lower() == "e" then
+    if flying then flying = false
+  else
+    flying = true
+    Fly()
+  end
+elseif key:lower() == "w" then
+  ctrl.f = 1
+elseif key:lower() == "s" then
+  ctrl.b = -1
+elseif key:lower() == "a" then
+  ctrl.l = -1
+elseif key:lower() == "d" then
+  ctrl.r = 1
+end
+end)
+mouse.KeyUp:connect(function(key)
+if key:lower() == "w" then
+  ctrl.f = 0
+elseif key:lower() == "s" then
+  ctrl.b = 0
+elseif key:lower() == "a" then
+  ctrl.l = 0
+elseif key:lower() == "d" then
+  ctrl.r = 0
+end
+end)
+Fly()
 end)
 end
